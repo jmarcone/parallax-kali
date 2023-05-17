8d0152ec6b9c1df1820b30cc369c00dc8eddfb72
jmarcone <marconi.julian@gmail.com>
2023-05-17 21:17:10 +0200

    git init

--- js/app.js
+++ js/app.js
@@ -25,6 +25,7 @@ function update(cursorPosition){
 
 update(0)
 
+
 window.addEventListener("mousemove", (e) => {
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
